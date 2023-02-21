a318a3985f7b8945a78db1762724339c8321c8a3
tyler khan <88749508+tylrk@users.noreply.github.com>
2023-02-21 19:27:14 +0000

    completed getChar kata

---
+++ 8kyu/getChar.js
@@ -0,0 +1,10 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
+
+const getChar = c => String.fromCharCode(c);
